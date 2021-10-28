c46c44657ef473fd6c8b9c7dba5ab72c3e4e7862
Brian Choi <63972691+BeeChoi@users.noreply.github.com>
2021-10-28 09:06:21 -0400

    Add files via upload

---
+++ WhileLoops/Investigate Loop.py
@@ -0,0 +1,28 @@
+number = 7
+print("I'm thinking of a number, can you guess it?")
+guess = int(input("What’s your guess? "))
+while guess != number:
+    print("Wrong! Guess again!")
+    guess = int(input("What’s your guess? "))
+print("You guessed it!")
+
+# Give the line number where iteration starts.
+  # 4
+
+# What does '!=' mean?
+  # Not Equal
+
+# How many variables are there in the code?
+  # 2
+
+# How can you tell which lines of code are inside the loop?
+  # They're tabbed
+
+# How many times will the loop repeat?
+  # Until the guess is equal to 7
+
+# What has to happen to make the program run the last line of code?
+  # Guess has to be equal to 7
+
+# What did you need to do to fix the code and why?
+  # Cast the second input as an integer
---
+++ WhileLoops/Modify Loop.py
@@ -0,0 +1,8 @@
+number = 6
+counter = 1
+
+while counter < 13:
+    product = number*counter
+    print(number, "X", counter, "=", product)
+    counter = counter + 2
+print("Loop has finished.")
