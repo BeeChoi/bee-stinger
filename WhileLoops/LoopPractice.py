909ab8608809a448d438a31e99b975b6ee8a533e
Brian Choi <63972691+BeeChoi@users.noreply.github.com>
2021-10-28 09:06:54 -0400

    Add files via upload

---
+++ WhileLoops/LoopPractice.py
@@ -0,0 +1,27 @@
+div = "---------------------------------"
+
+count = 26
+while count < 78:
+    print(count)
+    count+=5
+
+print(div)
+
+count2 = 4
+while count2 < 90:
+    print(count2)
+    count2 += 10
+
+print(div)
+
+count3 = 78
+while count3 > 26:
+    count3 -= 5
+print(count3)
+
+print(div)
+
+count4 = 80
+while count4 > -1:
+    count4 -= 10
+print(count4)
