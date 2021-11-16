34f72f901f79a50dafbd085e94b143d90304478b
Brian Choi <63972691+BeeChoi@users.noreply.github.com>
2021-11-16 09:19:57 -0500

    Create IntroToListsExitTicket.py

---
+++ lists/IntroToListsExitTicket.py
@@ -0,0 +1,13 @@
+vegetables = ["Spinach", "Lettuce", "Corn", "Potato", "Broccoli"]
+
+print(vegetables[-1])
+#Answer: Prints Broccoli
+
+print(vegetables[4])
+#Answer: Prints Broccoli
+
+print(vegetables[6])
+#Answer: Error
+
+print(vegetables[1])
+#Answer: Would print Lettuce but does not due to the error in line 9
