bb1e065c1be81427e9dd3e73a8277fd2d7880dfc
Brian Choi <63972691+BeeChoi@users.noreply.github.com>
2021-11-17 09:28:00 -0500

    Create CreatingAndSplicing.py

---
+++ lists/CreatingAndSplicing.py
@@ -0,0 +1,19 @@
+#List Creating
+list1 = list(range(6,26))
+print(list1)
+
+list2 = list(range(0,101,2))
+print(list2)
+
+#List Slicing
+days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
+print(days)
+
+weekend = days[5:7]
+print(weekend)
+
+beginWeek = days[0:3]
+print(beginWeek)
+
+endWeek = days[2:5]
+print(endWeek)
