44d500e026f05e9d14dded27529fda8501e962af
Brian Choi <63972691+BeeChoi@users.noreply.github.com>
2021-12-01 23:26:30 -0500

    Create IntroductionToLoops.py

---
+++ ForLoops/IntroductionToLoops.py
@@ -0,0 +1,11 @@
+names = ['Liz','Jill','Susan','Jimmy', 'Anthony']
+for name in names:
+  print(name)
+
+print("\n---------------------\n")
+
+times = [15, 30, 45, 60]
+for time in times:
+  print(6*time, "Miles")
+
+print("\n---------------------\n")
